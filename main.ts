0fbb740304a22f3c70d987427fa034ab25ec1a5c
DLN-002 <146394752+DLN-002@users.noreply.github.com>
2023-10-10 17:06:10 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,224 @@
+namespace SpriteKind {
+    export const coin = SpriteKind.create()
+    export const duck = SpriteKind.create()
+    export const ghost = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.ghost, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    pause(1000)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Render.jumpWithHeightAndDuration(mySprite, 5, 500)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.coin, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(otherSprite)
+})
+controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
+    Render.toggleViewMode()
+})
+let mySprite: Sprite = null
+info.setLife(3)
+tiles.setCurrentTilemap(tilemap`level1`)
+Render.setViewMode(ViewMode.raycastingView)
+mySprite = Render.getRenderSpriteInstance()
+let coin = sprites.create(img`
+    . . b b b b . . 
+    . b 5 5 5 5 b . 
+    b 5 d 3 3 d 5 b 
+    b 5 3 5 5 1 5 b 
+    c 5 3 5 5 1 d c 
+    c d d 1 1 d d c 
+    . f d d d d f . 
+    . . f f f f . . 
+    `, SpriteKind.coin)
+let coin2 = sprites.create(img`
+    . . b b b b . . 
+    . b 5 5 5 5 b . 
+    b 5 d 3 3 d 5 b 
+    b 5 3 5 5 1 5 b 
+    c 5 3 5 5 1 d c 
+    c d d 1 1 d d c 
+    . f d d d d f . 
+    . . f f f f . . 
+    `, SpriteKind.coin)
+let ghost2 = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111bf.......
+    ......fffcdb1bdffff.....
+    ....fc111cbfbfc111cf....
+    ....f1b1b1ffff1b1b1f....
+    ....fbfbffffffbfbfbf....
+    .........ffffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.ghost)
+let ghost3 = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111bf.......
+    ......fffcdb1bdffff.....
+    ....fc111cbfbfc111cf....
+    ....f1b1b1ffff1b1b1f....
+    ....fbfbffffffbfbfbf....
+    .........ffffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.ghost)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(8, 7))
+tiles.placeOnTile(coin, tiles.getTileLocation(11, 4))
+tiles.placeOnTile(coin2, tiles.getTileLocation(7, 7))
+tiles.placeOnTile(ghost2, tiles.getTileLocation(7, 9))
+tiles.placeOnTile(ghost3, tiles.getTileLocation(15, 13))
+animation.runImageAnimation(
+coin,
+[img`
+    . . b b b b . . 
+    . b 5 5 5 5 b . 
+    b 5 d 3 3 d 5 b 
+    b 5 3 5 5 1 5 b 
+    c 5 3 5 5 1 d c 
+    c d d 1 1 d d c 
+    . f d d d d f . 
+    . . f f f f . . 
+    `,img`
+    . . b b b . . . 
+    . b 5 5 5 b . . 
+    b 5 d 3 d 5 b . 
+    b 5 3 5 1 5 b . 
+    c 5 3 5 1 d c . 
+    c 5 d 1 d d c . 
+    . f d d d f . . 
+    . . f f f . . . 
+    `,img`
+    . . . b b . . . 
+    . . b 5 5 b . . 
+    . b 5 d 1 5 b . 
+    . b 5 3 1 5 b . 
+    . c 5 3 1 d c . 
+    . c 5 1 d d c . 
+    . . f d d f . . 
+    . . . f f . . . 
+    `,img`
+    . . . b b . . . 
+    . . b 5 5 b . . 
+    . . b 1 1 b . . 
+    . . b 5 5 b . . 
+    . . b d d b . . 
+    . . c d d c . . 
+    . . c 3 3 c . . 
+    . . . f f . . . 
+    `,img`
+    . . . b b . . . 
+    . . b 5 5 b . . 
+    . b 5 1 d 5 b . 
+    . b 5 1 3 5 b . 
+    . c d 1 3 5 c . 
+    . c d d 1 5 c . 
+    . . f d d f . . 
+    . . . f f . . . 
+    `,img`
+    . . . b b b . . 
+    . . b 5 5 5 b . 
+    . b 5 d 3 d 5 b 
+    . b 5 1 5 3 5 b 
+    . c d 1 5 3 5 c 
+    . c d d 1 d 5 c 
+    . . f d d d f . 
+    . . . f f f . . 
+    `],
+100,
+true
+)
+animation.runImageAnimation(
+coin2,
+[img`
+    . . b b b b . . 
+    . b 5 5 5 5 b . 
+    b 5 d 3 3 d 5 b 
+    b 5 3 5 5 1 5 b 
+    c 5 3 5 5 1 d c 
+    c d d 1 1 d d c 
+    . f d d d d f . 
+    . . f f f f . . 
+    `,img`
+    . . b b b . . . 
+    . b 5 5 5 b . . 
+    b 5 d 3 d 5 b . 
+    b 5 3 5 1 5 b . 
+    c 5 3 5 1 d c . 
+    c 5 d 1 d d c . 
+    . f d d d f . . 
+    . . f f f . . . 
+    `,img`
+    . . . b b . . . 
+    . . b 5 5 b . . 
+    . b 5 d 1 5 b . 
+    . b 5 3 1 5 b . 
+    . c 5 3 1 d c . 
+    . c 5 1 d d c . 
+    . . f d d f . . 
+    . . . f f . . . 
+    `,img`
+    . . . b b . . . 
+    . . b 5 5 b . . 
+    . . b 1 1 b . . 
+    . . b 5 5 b . . 
+    . . b d d b . . 
+    . . c d d c . . 
+    . . c 3 3 c . . 
+    . . . f f . . . 
+    `,img`
+    . . . b b . . . 
+    . . b 5 5 b . . 
+    . b 5 1 d 5 b . 
+    . b 5 1 3 5 b . 
+    . c d 1 3 5 c . 
+    . c d d 1 5 c . 
+    . . f d d f . . 
+    . . . f f . . . 
+    `,img`
+    . . . b b b . . 
+    . . b 5 5 5 b . 
+    . b 5 d 3 d 5 b 
+    . b 5 1 5 3 5 b 
+    . c d 1 5 3 5 c 
+    . c d d 1 d 5 c 
+    . . f d d d f . 
+    . . . f f f . . 
+    `],
+100,
+true
+)
+game.onUpdateInterval(1000, function () {
+	
+})
